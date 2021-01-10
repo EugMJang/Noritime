2d96a67a1ebabd90a441e1f45cc9f61ccee78984
Eugene Jang <eug.jang123@gmail.com>
2021-01-09 23:44:19 -0600

    Fix win screen

--- Assets/Scripts/Winner.cs
+++ Assets/Scripts/Winner.cs
@@ -8,12 +8,13 @@ public class Winner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Win").SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Score.blueScore);
         if (Score.redScore == 4) {
             GameObject.Find("Win").SetActive(true);
             GameObject.Find("Win").GetComponent<Text>().text = "Red Wins";
